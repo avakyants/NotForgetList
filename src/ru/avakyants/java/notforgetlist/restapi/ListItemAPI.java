003938979e55432b41ef96a20e2c721908ada161
tema14 <tema14@172.20.10.2>
2018-02-09 20:16:41 +0300

    try params

--- src/ru/avakyants/java/notforgetlist/restapi/ListItemAPI.java
+++ src/ru/avakyants/java/notforgetlist/restapi/ListItemAPI.java
@@ -6,7 +6,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
 import ru.avakyants.java.notforgetlist.model.ListItem;
 import ru.avakyants.java.notforgetlist.model.ListItemStatus;
@@ -17,10 +21,20 @@ public class ListItemAPI {
 	
 	/*
 	 * преобразует ответ из объекта в json
+	 * 
+	 * Объекты которые можно достать из запроса:
+	 * UriInfo, Request, HttpHeaders, SecurityContext, Providers
+	 * 
 	 * */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<ListItem> sayPlainTextHello(@PathParam("listid") long listId){
+	public List<ListItem> sayPlainTextHello(@PathParam("listid") long listId, @QueryParam("key") String key, @Context UriInfo uriInfo){
+		
+		System.out.println("KEY is: "+key);
+		//MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
+		List<String> keys = uriInfo.getQueryParameters().get("key");
+		keys.forEach(System.out::println);
+		//.forEach((k,v)->System.out.println(k+":"+v));
 		
 		ListItem i1 = new ListItem(1L,listId,"Item 1");
 		ListItem i2 = new ListItem(2L,listId,"Item 2",ListItemStatus.COMPLETED);
